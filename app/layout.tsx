4f6503ba37519d0634183b9a7ae9918edb271b64
D-Sewhuk <djs9142@rit.edu>
2026-04-08 08:31:46 -0400

    GTM snippets
    Added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
 })
-
 const instrumentSerif = Instrument_Serif({
   subsets: ['latin'],
   weight: '400',
@@ -25,7 +24,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${instrumentSerif.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9KTSL5L');` }} />
+        {/* End Google Tag Manager */}
+      </head>
       <body style={{ fontFamily: 'var(--font-inter), system-ui, sans-serif' }}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9KTSL5L" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
